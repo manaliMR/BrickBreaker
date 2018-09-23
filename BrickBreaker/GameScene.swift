9d567bf839fb06aaef30208e943fc400fcde6787
manali78 <manalirami78@gmail.com>
2018-09-22 23:01:19 -0400

    Condition for play again added

--- BrickBreaker/GameScene.swift
+++ BrickBreaker/GameScene.swift
@@ -13,11 +13,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var ball: SKSpriteNode!
     var paddle: SKSpriteNode!
+    var playAgainLabel: SKLabelNode!
+    
     
     override func didMove(to view: SKView) {
         ball = self.childNode(withName: "Ball") as! SKSpriteNode
         paddle = self.childNode(withName: "Paddle") as! SKSpriteNode
+        playAgainLabel = self.childNode(withName: "playAgain") as! SKLabelNode
         ball.physicsBody?.applyImpulse(CGVector(dx: 50, dy: 50))
+        
         let border = SKPhysicsBody(edgeLoopFrom: (view.scene?.frame)!)
         border.friction = 0
         self.physicsBody = border
@@ -37,14 +41,23 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            let touchLocation = touch.location(in: self)
-            if touchLocation.x > self.frame.width/2 - 100 {
-                paddle.position.x = self.frame.width/2 - 100
-            } else if touchLocation.x < -(self.frame.width/2) + 100 {
-                paddle.position.x = -(self.frame.width/2) + 100
-            } else {
-                paddle.position.x = touchLocation.x
+        
+        let touch = touches.first
+        let positionInScene = touch!.location(in:self)
+        let touchNode = self.atPoint(positionInScene)
+        
+        if touchNode.name == "playAgain" {
+            playAgain()
+        } else {
+            for touch in touches {
+               let touchLocation = touch.location(in: self)
+               if touchLocation.x > self.frame.width/2 - 100 {
+                  paddle.position.x = self.frame.width/2 - 100
+                } else if touchLocation.x < -(self.frame.width/2) + 100 {
+                  paddle.position.x = -(self.frame.width/2) + 100
+                } else {
+                  paddle.position.x = touchLocation.x
+                }
             }
         }
     }
@@ -56,5 +69,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         if firstObject == "Ball" && secondObject == "Brick" {
             contact.bodyB.node?.removeFromParent()
         }
+        if firstObject == "Ball" && secondObject == "DangerLine" {
+            contact.bodyA.velocity = CGVector(dx: 0, dy: 0)
+            playAgainLabel.isHidden = false
+        }
+    }
+    
+    func playAgain() {
+        self.removeAllChildren()
+        
+        let gameScene:GameScene = GameScene(fileNamed: "GameScene")!
+        let 
     }
 }
